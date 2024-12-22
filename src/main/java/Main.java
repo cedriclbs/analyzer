3b763e72c06e3783c0082906b8812c37b2a06e9f
lesbiens <cedric.les-biens@etu.u-paris.fr>
2024-12-22 19:51:04 +0100

    initialisation du projet - création des fichiers de base

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+package main.java;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        String samplePath = "src/main/resources/sample-corpus.txt";
+        CorpusAnalyzer corpusAnalyzer = new CorpusAnalyzer();
+    }
+}
